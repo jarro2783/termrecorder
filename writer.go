4185081d84e8c1f70af31018f27b9fa3369c30de
Jarryd Beck <jarro.2783@gmail.com>
2016-08-10 08:44:11 +1000

    basic writer

--- writer.go
+++ writer.go
@@ -1,21 +1,30 @@
 package termserver
 
 import "fmt"
-import "net"
+import fb "github.com/jarro2783/featherbyte"
 
 type Writer struct {
-    server net.Conn
+    endpoint *fb.Endpoint
+    reader fb.ReadData
+}
+
+type writeListener struct {
+}
+
+func (*writeListener) Data(messageType byte, data []byte) {
+}
+
+func (*writeListener) Message(messageType byte, data []byte) {
 }
 
 func CreateWriter(host string, port int, user string) (*Writer, error) {
-    connection, err := net.Dial("tcp", fmt.Sprintf("%s:%d", host, port))
-
     var writer *Writer
+    var err error
 
-    if connection != nil {
-        writer = new(Writer)
-        writer.server = connection
-    }
+    writer = new(Writer)
+    writer.reader = new(writeListener)
+    writer.endpoint, err = fb.Connect("tcp", fmt.Sprintf("%s:%d", host, port),
+        writer.reader)
 
     return writer, err
 }
