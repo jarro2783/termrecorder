ced22227831362f68401e017faf5149415c5683a
Jarryd Beck <jarro.2783@gmail.com>
2016-08-16 08:38:21 +1000

    publisher

--- termrecord_server/main.go
+++ termrecord_server/main.go
@@ -11,14 +11,23 @@ type subscribeRequest struct {
 
 type publishRequest struct {
     user string
-    response chan<- (chan<- []byte)
+    listeners chan<- (chan<- []byte)
 }
 
-type subscribeChannel <-chan subscribeRequest
-type publishChannel <-chan publishRequest
+func coordinator(subscribe <-chan subscribeRequest,
+    publish <-chan publishRequest) {
 
-func coordinator(subscribe subscribeChannel,
-    publish publishChannel) {
+    select {
+        case _, ok := <-subscribe:
+
+        if ok {
+        }
+
+        case _, ok := <-publish:
+
+        if ok {
+        }
+    }
 }
 
 func (handler *connectionHandler) Connection(*fb.Endpoint) {
@@ -26,6 +35,8 @@ func (handler *connectionHandler) Connection(*fb.Endpoint) {
 }
 
 type listener struct {
+    subscribe chan<- subscribeRequest
+    publish chan<- publishRequest
 }
 
 func (l *listener) Bytes(data []byte) {
@@ -34,35 +45,78 @@ func (l *listener) Bytes(data []byte) {
 
 func (l *listener) Send(user *termrecorder.UserRequest) {
     fmt.Printf("Got user request for %s\n", user.User)
+
+    var response chan chan<- []byte = make(chan chan<- []byte)
+
+    var pub = publishRequest {
+        user.User,
+        response,
+    }
+
+    l.publish <- pub
+
+    dataChannel := make(chan []byte)
+
+    go publisher(dataChannel, response)
+}
+
+func publisher(data <-chan []byte, register <-chan chan<- []byte) {
+    subscribers := make([](chan<- []byte), 0, 10)
+    for {
+        select {
+            case bytes, ok := <-data:
+
+            if !ok {
+                break
+            }
+
+            for s := range(subscribers) {
+                subscribers[s] <- bytes
+            }
+
+            case subscriber, ok := <-register:
+
+            if !ok {
+                break
+            }
+
+            subscribers = append(subscribers, subscriber)
+        }
+    }
 }
 
 func (l *listener) Watch(user *termrecorder.UserRequest) {
     fmt.Printf("Watch user")
 }
 
-type connectionHandler struct {
-    *listener
-    subscribe subscribeChannel
-    publish publishChannel
+func makeListener(subscribe chan<- subscribeRequest,
+    publish chan<- publishRequest) *listener {
+
+    l := new(listener)
+    l.subscribe = subscribe
+    l.publish = publish
+
+    return l
 }
 
-func makeHandler(listen *listener, subscribe subscribeChannel,
-    publish publishChannel) *connectionHandler {
+type connectionHandler struct {
+    *listener
+}
+
+func makeHandler(listen *listener) *connectionHandler {
     h := new(connectionHandler)
 
     h.listener = listen
-    h.subscribe = subscribe
-    h.publish = publish
 
     return h
 }
 
 func main() {
-    subscribe := make(subscribeChannel)
-    publish := make(publishChannel)
+    subscribe := make(chan subscribeRequest)
+    publish := make(chan publishRequest)
 
     go coordinator(subscribe, publish)
 
     termrecorder.Listen("localhost", 34234,
-        makeHandler(new(listener), subscribe, publish))
+        makeHandler(makeListener(subscribe, publish)))
 }
