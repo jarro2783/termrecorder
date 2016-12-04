c369bc3bd6eb2c03304d06cbd13e88e3136ab8c7
Jarryd Beck <jarro.2783@gmail.com>
2016-12-05 08:32:03 +1100

    start using a goroutine

--- termrecord_client/main.go
+++ termrecord_client/main.go
@@ -40,13 +40,15 @@ func (*WatchListener) Watch(*termrecorder.WatchRequest) {
 func (*WatchListener) Exiting() {
 }
 
+func sender(user, host string, port int, input <-chan []byte) {
+    //writer.Send(user, "")
+}
+
 func main() {
     user := flag.String("user", "", "The name of the user to record")
     host := flag.String("host", "", "The host to send the session to")
     port := flag.Int("port", 34234, "The port to connect to on the host")
     watch := flag.Bool("watch", false, "Watch the requested user")
-    gameid := flag.String("gameid", "",
-        "An optional game id if sending a session")
     send := flag.Bool("send", false,
         "Send a session for the requested user")
 
@@ -78,17 +80,10 @@ func main() {
         watcher = new(SendListener)
     }
 
-    //fmt.Printf("Session for %s connecting to %s:%d\n", *user, *host, *port)
-
-    writer, err := termrecorder.Connect(*host, *port, watcher)
-
-    if err != nil {
-        fmt.Printf("Error connecting to host: %s\n", err.Error())
-        os.Exit(1)
-    }
-
     if (*send) {
-        writer.Send(*user, *gameid)
+        sendChannel := make(chan []byte, 100)
+
+        go sender(*user, *host, *port, sendChannel)
 
         var data []byte = make([]byte, 1024)
         for true {
@@ -99,10 +94,17 @@ func main() {
             }
 
             if n != 0 {
-                writer.Write(data[0: n])
+                sendChannel <- data[0:n]
             }
         }
     } else {
+        writer, err := termrecorder.Connect(*host, *port, watcher)
+
+        if err != nil {
+            fmt.Printf("Error connecting to host: %s\n", err.Error())
+            os.Exit(1)
+        }
+
         writer.Watch(*user)
         var data[1]byte
         os.Stdin.Read(data[:])
